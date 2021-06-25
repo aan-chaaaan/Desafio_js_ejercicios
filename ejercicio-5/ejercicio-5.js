24cfc41792cc98187f58b8fd8eed6ffedd77d431
Angela <angela.valdebenito.v@gmail.com>
2021-06-25 18:22:55 -0400

    commit2

--- ejercicio-5/ejercicio-5.js
+++ ejercicio-5/ejercicio-5.js
@@ -1,8 +1,8 @@
-let num1 = prompt("ingrese un primer numero");
-let num2 = prompt("ingrese un segundo numero");
-let num3 = prompt("ingrese un tercer numero");
-let num4 = prompt("ingrese un cuarto numero");
-let num5 = prompt("y un quinto numero");
+let num1 = prompt("Ingrese un primer numero");
+let num2 = prompt("Ingrese un segundo numero");
+let num3 = prompt("Ingrese un tercer numero");
+let num4 = prompt("Ingrese un cuarto numero");
+let num5 = prompt("Y un quinto numero");
 
 
 let sumatoria = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4) + parseInt(num5);
